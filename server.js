29583fb3452576c846fdc469aadf1052027848c1
‘Timothy <Timothy.viccari@gmail.com>
2021-04-20 18:28:32 -0400

    added Forecast constructor function

--- server.js
+++ server.js
@@ -6,8 +6,26 @@ const cors = require('cors');
 
 const app = express();
 
+const weather = require('./data/weather.json');
+
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.get('/', (request, response) => {
+  response.send('SUP from the home route!');
+});
+
+app.get('/weather', (request, response) => {
+  response.send(weather);
+});
+
+function Forecast(day) {
+  this.date = day.valid_date;
+  this.description = day.weather.description;
+
+}
+
+
